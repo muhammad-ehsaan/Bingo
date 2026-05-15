bf4dd3546de6c90beb37e556e7e780a163cd41e6
Noor Ullah <itxnoor148@gmail.com>
2026-05-15 22:42:56 +0500

    Updated

--- mycode.asm
+++ mycode.asm
@@ -1,52 +1,128 @@
-DisplayCard PROC
-    PUSH AX
-    PUSH BX
-    PUSH CX
-    PUSH DX
-    PUSH SI
+PrintNum proc
+    push ax
+    push bx
+    push dx
 
-    MOV BX, 0
-    MOV CX, 5              ; 5 rows
+    mov ah, 0
+    mov bl, 10
+    div bl                ; AL=tens, AH=ones
 
-ROWLOOP:
-    PUSH CX
-    MOV CX, 5              ; 5 cols
+    ; Print tens digit
+    push ax
+    mov dl, al
+    add dl, 48            ; convert to ASCII like lab 3
+    mov ah, 02h
+    int 21h
+    pop ax
 
-COLLOOP:
-    MOV SI, markPtr
-    CMP BYTE PTR [SI+BX], 1
-    JNE SHOWNUM
+    ; Print ones digit
+    mov dl, ah
+    add dl, 48
+    mov ah, 02h
+    int 21h
 
-    ; Marked cell ? print " *  "
-    MOV DL, ' '
-    MOV AH, 02H
-    INT 21H
-    MOV DL, '*'
-    MOV AH, 02H
-    INT 21H
-    MOV DL, ' '
-    MOV AH, 02H
-    INT 21H
-    JMP CELLDONE
+    ; Space after number
+    PCHAR ' '
 
-SHOWNUM:
-    MOV SI, cardPtr
-    MOV AL, [SI+BX]
-    CALL PrintNumber
+    pop dx
+    pop bx
+    pop ax
+    ret
+PrintNum endp
 
-CELLDONE:
-    INC BX
-    LOOP COLLOOP
+; PROC: DisplayCards
+; Shows both 5x5 cards using nested loops
+; Outer loop = rows (saved with push cx like Lab 7)
+; Inner loop = columns
 
-    NEWLINE
-    POP CX
-    LOOP ROWLOOP
+DisplayCards proc
+    push ax
+    push bx
+    push cx
+    push dx
+    push si
 
-    NEWLINE
-    POP SI
-    POP DX
-    POP CX
-    POP BX
-    POP AX
-    RET
-DisplayCard ENDP
+    ; --- Player 1 Card ---
+    PNEWLINE
+    printn '--- PLAYER 1 ---'
+    printn '----------------'
+
+    mov si, 0             ; si = cell index
+    mov bx, 5             ; bx = outer loop counter (rows)
+
+D1Outer:
+    mov cx, 5             ; cx = inner loop (columns)
+D1Inner:
+    push cx               ; save inner cx (nested loop - Lab 7 style)
+
+    ; Check if marked
+    mov al, mark1[si]
+    cmp al, 1
+    je D1Marked
+
+    ; Not marked: print number
+    mov al, card1[si]
+    call PrintNum
+    jmp D1Next
+
+D1Marked:
+    ; Print "XX " for marked
+    PCHAR 'X'
+    PCHAR 'X'
+    PCHAR ' '
+
+D1Next:
+    inc si
+    pop cx
+    loop D1Inner
+
+    PNEWLINE
+    dec bx
+    cmp bx, 0
+    jne D1Outer
+
+    ; --- Player 2 Card ---
+    PNEWLINE
+    printn '--- PLAYER 2 ---'
+    printn '----------------'
+
+    mov si, 0
+    mov bx, 5
+
+D2Outer:
+    mov cx, 5
+D2Inner:
+    push cx
+
+    mov al, mark2[si]
+    cmp al, 1
+    je D2Marked
+
+    mov al, card2[si]
+    call PrintNum
+    jmp D2Next
+
+D2Marked:
+    PCHAR 'X'
+    PCHAR 'X'
+    PCHAR ' '
+
+D2Next:
+    inc si
+    pop cx
+    loop D2Inner
+
+    PNEWLINE
+    dec bx
+    cmp bx, 0
+    jne D2Outer
+
+    PNEWLINE
+
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+DisplayCards endp
